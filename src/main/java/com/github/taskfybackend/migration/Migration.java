c3d775df5a3ee0c717c07f96fe000ebed37998b1
JiLaRiAtiOn <jilariation@gmail.com>
2024-03-09 15:45:33 +0300

    Update main branch

--- src/main/java/com/github/taskfybackend/migration/Migration.java
+++ src/main/java/com/github/taskfybackend/migration/Migration.java
@@ -11,11 +11,18 @@ public class Migration {
     private final UserRepository userRepository;
 
     public void migration() {
+        deleteAllCollections();
         createCollectionUsers();
+    }
+
+    private void deleteAllCollections() {
+        userRepository.deleteAll();
+        System.out.println("Все коллекции удалены");
     }
 
     private void createCollectionUsers() {
         var user = User.builder().username("Oleg").email("oleg@gmail.com").password("123456789").build();
         userRepository.save(user);
+        System.out.println("Коллекция users успешно добавлена");
     }
 }
